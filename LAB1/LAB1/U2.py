7bb9a51d0cd62f55b45ab66016351d907d48aaf3
aletricca02 <aletricca18@gmail.com>
2025-03-25 17:18:22 +0100

    first commit

---
+++ LAB1/LAB1/U2.py
@@ -0,0 +1,8 @@
+
+from utilities.search import *
+from collections import defaultdict, deque, Counter
+from utilities.pourproblem import *
+from utilities.reporting import *
+
+class U2(Problem):
+    ...
